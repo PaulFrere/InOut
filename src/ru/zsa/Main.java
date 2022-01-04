6a59d5756645c7bfceecb39482cd946191730c8f
PaulFrere <paulfrere91@gmail.com>
2022-01-04 15:08:27 +0700

    try-with-resources

--- src/ru/zsa/Main.java
+++ src/ru/zsa/Main.java
@@ -6,33 +6,34 @@ import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args){
+
       //Запуск для первой задачи
         File save = new File("progress.txt");
+        try  (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(save))) {
+            oos.writeObject(new Progress("Gordon", 88, 256, 4));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(save));
-        oos.writeObject(new Progress("Gordon", 88, 256, 4));
-        oos.close();
-
-        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(save));
-        Progress progress = (Progress) ois.readObject();
-        System.out.println(progress);
-        ois.close();
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(save))){
+            Progress progress = (Progress) ois.readObject();
+            System.out.println(progress);
+        } catch (ClassNotFoundException | IOException e) {
+            e.printStackTrace();
+        }
 
         // работа с файлом
         File file = new File((String) null);
 
-        BufferedReader pic = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-        File picture = new File(String.valueOf(pic));
-
-        FileOutputStream newPicture = new FileOutputStream(picture, false);
-
+        try (BufferedReader pic = new BufferedReader(new InputStreamReader(new FileInputStream(file)))){
+            File picture = new File(String.valueOf(pic));
+            FileOutputStream newPicture = new FileOutputStream(picture, false);
             byte[] myBytes = "New Picture\n".getBytes();
-
-               newPicture.write(myBytes);
-
-               newPicture.close();
-
-        System.out.println(newPicture);
+            newPicture.write(myBytes);
+            System.out.println(newPicture);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
